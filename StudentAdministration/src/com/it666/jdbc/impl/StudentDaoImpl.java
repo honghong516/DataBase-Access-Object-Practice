3e6f271fc624c89f965ed2e41fe1b67a5c722514
honghong516 <44778347+honghong516@users.noreply.github.com>
2020-01-04 16:52:00 +0800

    student project for jdbc practice

---
+++ StudentAdministration/src/com/it666/jdbc/impl/StudentDaoImpl.java
@@ -0,0 +1,134 @@
+package com.it666.jdbc.impl;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.it666.jdbc.dao.IStudent;
+import com.it666.jdbc.domain.Student;
+import com.it666.jdbc.util.DbUtil;
+
+public class StudentDaoImpl implements IStudent {
+	// load params from conf
+	
+	@Override
+	public void save(Student su) throws IOException {
+		// TODO Auto-generated method stub
+		if(su==null) {
+			System.out.println("object is null, can not insert into table");
+			return;
+		}
+		Connection conn =  DbUtil.getConnection();
+		System.out.println(conn);
+		if(conn == null) {
+			System.out.println("get connection failed");
+			throw new IOException("get connection failed");
+		}
+		PreparedStatement prepareStatement = null;
+		String sql = "insert into student (name, age, update_time) values(?,?,?)";
+		try {
+			prepareStatement = conn.prepareStatement(sql);
+			prepareStatement.setString(1, su.getName());
+			prepareStatement.setInt(2, su.getAge());
+			prepareStatement.setString(3, su.getUpdate_time());
+			int executeUpdate = prepareStatement.executeUpdate();
+			System.out.println("execute result: " + executeUpdate);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			DbUtil.close(conn, prepareStatement, null);
+		}
+	}
+
+	@Override
+	public void update(Student su) throws IOException {
+		// TODO Auto-generated method stub
+		if(su==null) {
+			System.out.println("object is null, can not update table");
+			return;
+		}
+		Connection conn =  DbUtil.getConnection();
+		System.out.println(conn);
+		if(conn == null) {
+			System.out.println("get connection failed");
+			throw new IOException("get connection failed");
+		}
+		PreparedStatement prepareStatement = null;
+		String sql = "update student set name=? where id=?";
+		try {
+			prepareStatement = conn.prepareStatement(sql);
+			prepareStatement.setString(1, su.getName());
+			prepareStatement.setInt(2, su.getId());
+			int executeUpdate = prepareStatement.executeUpdate();
+			System.out.println("execute result: " + executeUpdate);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			DbUtil.close(conn, prepareStatement, null);
+		}
+	}
+
+	@Override
+	public void delete(int id) throws IOException {
+		// TODO Auto-generated method stub
+		Connection conn =  DbUtil.getConnection();
+		System.out.println(conn);
+		if(conn == null) {
+			System.out.println("get connection failed");
+			throw new IOException("get connection failed");
+		}
+		PreparedStatement prepareStatement = null;
+		String sql = "delete from student where id=?";
+		try {
+			prepareStatement = conn.prepareStatement(sql);
+			prepareStatement.setInt(1, id);
+			int executeUpdate = prepareStatement.executeUpdate();
+			System.out.println("execute result: " + executeUpdate);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			DbUtil.close(conn, prepareStatement, null);
+		}		
+	}
+
+	@Override
+	public List<Student> query(String name) throws IOException {
+		// TODO Auto-generated method stub
+		Connection conn =  DbUtil.getConnection();
+		ArrayList<Student> list = new ArrayList<Student>();
+		System.out.println(conn);
+		if(conn == null) {
+			System.out.println("get connection failed");
+			throw new IOException("get connection failed");
+		}
+		PreparedStatement prepareStatement = null;
+		String sql = "select * from student where name like ?";
+		try {
+			prepareStatement = conn.prepareStatement(sql);
+			prepareStatement.setString(1,name);
+			ResultSet result = prepareStatement.executeQuery();
+			while(result.next()) {
+				Student su = new Student();
+				su.setId(result.getInt(1));
+				su.setName(result.getString(2));
+				su.setAge(result.getInt(3));
+				su.setUpdate_time(result.getString(4));
+				list.add(su);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			DbUtil.close(conn, prepareStatement, null);
+		}			
+		return list;
+	}
+
+}
---
+++ StudentAdministration/src/com/it666/jdbc/test/StudentDaoTest.java
@@ -0,0 +1,79 @@
+package com.it666.jdbc.test;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.it666.jdbc.dao.IStudent;
+import com.it666.jdbc.domain.Student;
+import com.it666.jdbc.impl.StudentDaoImpl;
+import com.it666.jdbc.util.DbUtil;
+
+public class StudentDaoTest {
+	
+	@Test
+	public void deleteTest() {
+		Student student = new Student();
+		student.setName("honghong1");
+		student.setId(1);
+		IStudent dao = new StudentDaoImpl();
+		try {
+			dao.delete(2);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}				
+	}
+	@Test
+	public void updateTest() {
+		Student student = new Student();
+		student.setName("honghong1");
+		student.setId(1);
+		IStudent dao = new StudentDaoImpl();
+		try {
+			dao.update(student);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	@Test
+	public void saveTest() {
+		Student student = new Student();
+		student.setName("honghong2");
+		student.setAge(20);
+		student.setUpdate_time(DbUtil.getTime());
+		IStudent dao = new StudentDaoImpl();
+		try {
+			dao.save(student);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void queryTest() {
+		IStudent dao = new StudentDaoImpl();
+		List<Student> list = null;
+		try {
+			list = dao.query("honghong%");
+			for(Student su:list) {
+				System.out.println(su);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
+	@Test
+	public void implTest() {
+		IStudent studentDaoImpl = new StudentDaoImpl();
+		
+	}
+	
+	public static void main(String[] args) {
+		new DbUtil();
+	}
+}
